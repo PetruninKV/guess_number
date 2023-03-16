0559b77ef07491f9b87fccd953be25d2e3cb507a
Kirill Petrunin <petruninkir@gmail.com>
2023-03-16 20:22:06 +0400

    add_numbers/other_answer

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import random
 import os
 from dotenv import load_dotenv
 
+import messages_text
+from config import ATTEMPTS, USER
 
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import Text, Command
-import messages_text
-from config import ATTEMPTS, USER
+
 
 load_dotenv()
 
@@ -60,10 +61,38 @@ async def negative_answer(message: Message):
         await message.answer(messages_text.ERROR_MES_IN_GAME2)
 
 
+@dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
+async def numbers_answer(message: Message):
+    if USER['in_game']:
+        number = int(message.text)
+        if number == USER['secret_number']:
+            await message.answer('Поздравляю! Вы угадали! Может еще разок?')
+            USER["in_game"] = False
+            USER['wins'] += 1
+            USER['total_games'] += 1
+        elif number < USER['secret_number']:
+            await message.answer('Загаданное число больше')
+            USER['attempts'] -= 1
+        elif number > USER['secret_number']:
+            await message.answer('Загаданное число меньше')
+            USER['attempts'] -= 1
+
+        if USER['attempts'] == 0:
+            await message.answer(messages_text.GAME_OVER)
+            USER['in_game'] = False
+            USER['total_games'] += 1
+
+    else:
+        await message.answer('Мы еще не играем. Хотите сыграть? Отправьте положительный ответ')
 
 
+@dp.message()
+async def other_anser(message: Message):
+    if USER['in_game']:
+        await message.answer('Мы же сейчас играем! Присылайте выши числа.')
+    else:
+        await message.answer('Давайте лучше сыграем в игру!')
 
 
 if __name__ == "__main__":
     dp.run_polling(bot)
-
