8352d29bcaec774b6de404a2b9c4808bcb1eb606
Kirill Petrunin <petruninkir@gmail.com>
2023-03-16 18:40:32 +0400

    add_commands_stat,cancel

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import Text, Command
 import messages_text
+import config
 
 load_dotenv()
 
@@ -14,23 +15,6 @@ TOKEN: str = os.getenv("TOKEN")
 
 bot: Bot = Bot(token=TOKEN)
 dp: Dispatcher = Dispatcher()
-
-ATTEMPTS: int = 5
-USER: dict = {
-            'in_game': False,
-            'secret_number': None,
-            'attempts': None,
-            'total_games': 0,
-            'wins': 0
-            }
-
-
-
-
-
-
-
-
 
 
 
@@ -42,6 +26,29 @@ async def start_comand(message: Message):
 @dp.message(Command(commands=['help']))
 async def help_command(message: Message):
     await message.answer(messages_text.COMMAND_HELP)
+
+
+@dp.message(Command(commands=['stat']))
+async def stat_comand(message: Message):
+    await message.answer(messages_text.COMMAND_STAT)
+
+
+@dp.message(Command(commands=['cancel']))
+async def cancle_comand(message: Message):
+    pass
+
+
+@dp.message(Text(text=messages_text.POSITIVE_RESPONSES, ignore_case=True))
+async def positive_answer(message: Message):
+    pass
+
+
+
+@dp.message(Text(text=messages_text.NEGATIVE_RESPONSES, ignore_case=True))
+async def negative_answer(message: Message):
+    await message.answer('отрицательный ответ')
+
+
 
 
 
